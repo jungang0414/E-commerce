0a4af21d45d68065d0268ff7450567262c8accc6
jungang0414 <dayr88520@gmail.com>
2024-11-11 18:24:36 +0800

    feat: 新增 Home 部份組件 components: BestSeller

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,44 @@
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const BestSeller = () => {
+  const { products } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
+
+  // 依賴數組為[], 僅在首次渲染組件時執行一次
+  useEffect(() => {
+    // 這裡篩選出資料中 bestseller 為真的資料 儲存在陣列中,
+    // 接著取出陣列中前5個元素.
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+          id?
+        </p>
+      </div>
+
+      {/* 將 bestSeller 的資料傳遞給 ProductItem */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {bestSeller.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Hero from "../components/Hero";
+import BestSeller from "../components/BestSeller";
 import LatestCollection from "../components/LatestCollection";
+import OurPolicy from "../components/OurPolicy";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
+      <OurPolicy />
     </div>
   );
 };
