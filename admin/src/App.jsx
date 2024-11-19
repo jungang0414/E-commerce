774417ae746249befa117ae70e0080ffde56a29c
jungang0414 <dayr88520@gmail.com>
2024-11-19 10:52:16 +0800

    feat: admin 後端登入頁面
    1. 新增 Navbar、Sidebar、Login 共用組件
    2. 新增環境變數: 後端api (.gitignore記得取消追蹤.env 避免將隱私資料上傳Github)
    3. 引入google font字型
    4. 下載 toastify 優化彈出效果套件
    利用後端api取得token, 並藉由token驗證身分、渲染頁面.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,54 @@
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import { useEffect, useState } from "react";
+import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// 後端API
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
+  // 令牌狀態
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+
+  // 當依賴數組 token 變更則執行 useEffect 中的依賴函數 將 token 儲存到本地
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
   return (
-    <>
-      <Navbar />
-    </>
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {/* 這裡使用三元運算子 
+      當 token 為空值時顯示登入組件
+      反之若是有 token 值則顯示後端頁面 */}
+      {token === "" ? (
+        // 這裡將 setToken函數傳遞給 Login 組件使用
+        // 在 Login 組件中設定 Token
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import { assets } from "../assets/assets.js";
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={assets.logo} alt="logo" />
-      <button>Logout</button>
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      <img className="w-[max(10%,80px)]" src={assets.logo} alt="logo" />
+      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+        Logout
+      </button>
     </div>
   );
 };
